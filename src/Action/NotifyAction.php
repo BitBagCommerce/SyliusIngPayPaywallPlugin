3fc837da173475f61e5fdd7dca40e870765a63d8
Marcin Kukliński <senghe@gmail.com>
2025-12-03 08:35:41 +0100

    Handle failed and pending payment statuses

--- src/Action/NotifyAction.php
+++ src/Action/NotifyAction.php
@@ -12,12 +12,16 @@ namespace BitBag\SyliusImojePlugin\Action;
 
 use BitBag\SyliusImojePlugin\Api\ImojeApi;
 use BitBag\SyliusImojePlugin\Resolver\SignatureResolverInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Payum\Core\Action\ActionInterface;
 use Payum\Core\ApiAwareInterface;
 use Payum\Core\ApiAwareTrait;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\Request\Notify;
+use Sylius\Component\Core\Model\PaymentInterface;
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+use Sylius\Component\Payment\Factory\PaymentFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -30,11 +34,15 @@ final class NotifyAction implements ActionInterface, ApiAwareInterface
     public function __construct(
         private RequestStack $requestStack,
         private SignatureResolverInterface $signatureResolver,
+        private PaymentFactoryInterface $paymentFactory,
+        private EntityManagerInterface $entityManager,
+        private OrderRepositoryInterface $orderRepository,
     ) {
         $this->request = $requestStack->getCurrentRequest();
         $this->apiClass = ImojeApi::class;
     }
 
+    /** @param Notify $request */
     public function execute($request): void
     {
         RequestNotSupportedException::assertSupports($this, $request);
@@ -43,13 +51,37 @@ final class NotifyAction implements ActionInterface, ApiAwareInterface
             throw new \Exception('Request is empty');
         }
 
+        /** @var PaymentInterface $payment */
+        $payment = $request->getFirstModel();
+
         /** @var string $content */
         $content = $this->request->getContent();
         $notificationData = json_decode($content, true);
+        if ($notificationData['payment']['status'] === 'pending') {
+            return;
+        }
+
         $transactionData = $notificationData['transaction'];
 
         $model = $request->getModel();
         $model['statusImoje'] = $transactionData['status'];
+
+        if ($notificationData['payment']['status'] === 'error') {
+            /** @var PaymentInterface $newPayment */
+            $newPayment = $this->paymentFactory->createNew();
+            $newPayment->setState('new');
+            $newPayment->setCurrencyCode($payment->getCurrencyCode());
+            $newPayment->setAmount($payment->getAmount());
+            $this->entityManager->persist($newPayment);
+
+            $order = $payment->getOrder();
+            $reloadedOrder = $this->orderRepository->findOneBy(['id' => $order->getId()]);
+
+            $reloadedOrder->getLastPayment()->setState('failed');
+            $reloadedOrder->addPayment($newPayment);
+
+            $this->entityManager->flush();
+        }
 
         $request->setModel($model);
     }
